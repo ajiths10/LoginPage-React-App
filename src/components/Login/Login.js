2890f8452428f93cc6421252a9327f4978177b1c
Nuclear One <ajithsnair10@gmail.com>
2022-03-29 10:28:53 +0530

    Forward Refs

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useContext } from "react";
+import React, { useState, useEffect, useReducer, useContext, useRef } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
@@ -59,6 +59,10 @@ const Login = (props) => {
   });
 
   const authCtx = useContext(AuthContext);
+
+  const emailInputRef = useRef();
+  const collageInputRef = useRef();
+  const passwordInputRef = useRef();
 
   //to uderStand useEffect
   useEffect(() => {
@@ -126,13 +130,24 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, collageState.value, passwordState.value);
+    if(formIsValid){
+      authCtx.onLogin(emailState.value, collageState.value, passwordState.value);
+    } else if(!emailIsValid){
+      emailInputRef.current.focus();
+    }else if(!collageIsValid){
+        collageInputRef.current.focus();
+    }
+    else{
+      passwordInputRef.current.focus();
+    }
+    
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+        ref={emailInputRef}
           id="email"
           label="E-Mail"
           type="email"
@@ -142,6 +157,7 @@ const Login = (props) => {
           onBlur={validateEmailHandler}
         />
         <Input
+        ref={collageInputRef}
           id="collage"
           label="collage"
           type="text"
@@ -151,6 +167,7 @@ const Login = (props) => {
           onBlur={validateCollageHandler}
         />
         <Input
+        ref={passwordInputRef}
           id="password"
           label="password"
           type="password"
@@ -160,7 +177,7 @@ const Login = (props) => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn} >
             Login
           </Button>
         </div>
